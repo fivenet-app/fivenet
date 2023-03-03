8996e5d7d65e336396380ed5be06a45dcb9537d5
Alexander Trost <galexrt@googlemail.com>
2023-03-03 23:58:46 +0100

    add notifications to grpcauth
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/grpcauth.ts
+++ src/grpcauth.ts
@@ -2,6 +2,7 @@ import store from './store';
 import { RpcError, StatusCode, StreamInterceptor, UnaryInterceptor } from 'grpc-web';
 import { _RouteLocationBase } from 'vue-router/auto';
 import router from './main';
+import { dispatchNotification } from './components/Notification';
 
 class AuthInterceptor implements StreamInterceptor<any, any>, UnaryInterceptor<any, any> {
 	intercept(request: any, invoker: any) {
@@ -13,12 +14,14 @@ class AuthInterceptor implements StreamInterceptor<any, any>, UnaryInterceptor<a
 	}
 	handleError(err: RpcError, route: _RouteLocationBase): boolean {
 		if (err.code == StatusCode.UNAUTHENTICATED) {
-			store.commit('updateAccessToken', null);
-			// TODO show notification/ toast
-			router.push({ name: 'login', query: { redirect: route.fullPath } });
+			store.commit('doLogout');
+
+            router.push({ name: 'login', query: { redirect: route.fullPath } });
+
+            dispatchNotification({ title: 'Please login again!', content: 'You are not signed in anymore', type: 'warning' });
 			return true;
 		} else if (err.code == StatusCode.PERMISSION_DENIED) {
-			// TODO show notification/ toast
+            dispatchNotification({ title: 'Error!', content: err.message, type: 'error' });
 			return true;
 		}
 
