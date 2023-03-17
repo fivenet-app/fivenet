ed98f73f24b080e7348cabde16553aa53ca13b9d
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-17 19:26:34 +0100

    Fixed zindex of Notifications

--- src/components/notification/NotificationProvider.vue
+++ src/components/notification/NotificationProvider.vue
@@ -5,7 +5,7 @@ import store from './store';
 
 <template>
     <!-- Global notification live region, render this permanently at the end of the document -->
-    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50">
         <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
             <Notification :key="notification.id" :notification="notification" :class="idx > 0 ? 'mt-4' : ''"
                 v-for="(notification, idx) in store.getters.getNotifications()" />
