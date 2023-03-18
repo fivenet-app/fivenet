a26a6476f84efaaf16ee6f7fdaa72fa58f48f8f2
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-18 20:38:11 +0100

    Move AccessEntry into partial

--- src/components/documents/DocumentEditor.vue
+++ src/components/documents/DocumentEditor.vue
@@ -1,46 +1,21 @@
 <script lang="ts">
-import { defineComponent, ref, computed, ComputedRef } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { mapState } from 'vuex';
 import { Quill, QuillEditor } from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
-import { getDocStoreClient, handleGRPCError, getCompletorClient } from '../../grpc';
+import { getDocStoreClient, handleGRPCError } from '../../grpc';
 import { CreateDocumentRequest } from '@arpanet/gen/services/docstore/docstore_pb';
 import { DocumentAccess, DocumentJobAccess, DOC_ACCESS, DOC_CONTENT_TYPE } from '@arpanet/gen/resources/documents/documents_pb';
 import { RpcError } from 'grpc-web';
 import { dispatchNotification } from '../notification';
 import { User } from '@arpanet/gen/resources/users/users_pb';
-import { PlusIcon, CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
-import { watchDebounced } from '@vueuse/core';
-import { CompleteCharNamesRequest, CompleteJobNamesRequest } from '@arpanet/gen/services/completor/completor_pb';
-import {
-    Listbox,
-    ListboxButton,
-    ListboxLabel,
-    ListboxOption,
-    ListboxOptions,
-    Combobox,
-    ComboboxButton,
-    ComboboxInput,
-    ComboboxOption,
-    ComboboxOptions
-} from '@headlessui/vue'
+import AccessEntry from '../partials/AccessEntry.vue';
+
 
 export default defineComponent({
     components: {
         QuillEditor,
-        PlusIcon,
-        CheckIcon,
-        ChevronUpDownIcon,
-        Listbox,
-        ListboxButton,
-        ListboxLabel,
-        ListboxOption,
-        ListboxOptions,
-        Combobox,
-        ComboboxButton,
-        ComboboxInput,
-        ComboboxOption,
-        ComboboxOptions,
+        AccessEntry,
     },
     data() {
         return {
@@ -51,14 +26,6 @@ export default defineComponent({
             closed: false,
             state: "",
             public: false,
-            accessTypes: [
-                { id: 0, name: 'Citizen' },
-                { id: 1, name: 'Jobs' },
-            ],
-            selectedAccessType: ref<null | { id: number, name: string }>(null),
-            people: [] as { id: string | number, label: string }[],
-            query: { value: '' },
-            selectedPerson: ref(null),
         };
     },
     computed: {
@@ -76,17 +43,6 @@ export default defineComponent({
             modules,
         };
     },
-    mounted() {
-        this.selectedAccessType = this.accessTypes[1];
-
-        watchDebounced(this.query, () => {
-            if (this.selectedAccessType?.id === 0) {
-                this.findChars();
-            } else {
-                this.findJobs();
-            }
-        }, { debounce: 750, maxWait: 2000 });
-    },
     props: {
         'targetDocumentID': {
             required: false,
@@ -95,34 +51,6 @@ export default defineComponent({
         },
     },
     methods: {
-        findJobs(): void {
-            const req = new CompleteJobNamesRequest();
-            req.setSearch(this.query.value);
-
-            getCompletorClient().completeJobNames(req, null).then((resp) => {
-                this.people = [];
-                resp.getJobsList().forEach((job) => {
-                    this.people.push({ id: job.getName(), label: job.getLabel() })
-                })
-                console.log(this.people);
-            }).catch((err: RpcError) => {
-                handleGRPCError(err, this.$route);
-            })
-        },
-        findChars(): void {
-            const req = new CompleteCharNamesRequest();
-            req.setSearch(this.query.value);
-
-            getCompletorClient().completeCharNames(req, null).then((resp) => {
-                this.people = [];
-                resp.getUsersList().forEach((user) => {
-                    this.people.push({ id: user.getUserId(), label: `${user.getFirstname()} ${user.getLastname()}` })
-                })
-                console.log(this.people);
-            }).catch((err: RpcError) => {
-                handleGRPCError(err, this.$route);
-            })
-        },
         submitForm(): void {
             if (this.saving) {
                 return;
@@ -186,79 +114,7 @@ export default defineComponent({
     </div>
     <div class="my-3">
         <h2 class="text-neutral">Access</h2>
-        <div class="flex flex-col">
-            <div class="flex-1">
-                <Listbox as="div" v-model="selectedAccessType">
-                    <ListboxLabel class="block text-sm font-medium leading-6 text-neutral">Type</ListboxLabel>
-                    <div class="relative mt-2">
-                        <ListboxButton
-                            class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            <span class="block truncate">{{ selectedAccessType?.name }}</span>
-                            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                            </span>
-                        </ListboxButton>
-
-                        <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
-                            leave-to-class="opacity-0">
-                            <ListboxOptions
-                                class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                <ListboxOption as="template" v-for="accessType in accessTypes" :key="accessType.id"
-                                    :value="accessType" v-slot="{ active, selected }">
-                                    <li
-                                        :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-8 pr-4']">
-                                        <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{
-                                            accessType.name
-                                        }}</span>
-
-                                        <span v-if="selected"
-                                            :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 left-0 flex items-center pl-1.5']">
-                                            <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                                        </span>
-                                    </li>
-                                </ListboxOption>
-                            </ListboxOptions>
-                        </transition>
-                    </div>
-                </Listbox>
-            </div>
-            <div class="flex-1">
-                <Combobox as="div" v-model="selectedPerson">
-                    <div class="relative mt-2">
-                        <ComboboxInput
-                            class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            @change="query.value = $event.target.value" @click="selectedAccessType?.id === 0 ? findChars() : findJobs()" :display-value="(person: any) => person?.label" />
-                        <ComboboxButton
-                            class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                            <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-                        </ComboboxButton>
-
-                        <ComboboxOptions v-if="people.length > 0"
-                            class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            <ComboboxOption v-for="person in people" :key="person.id" :value="person" as="template"
-                                v-slot="{ active, selected }">
-                                <li
-                                    :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
-                                    <span :class="['block truncate', selected && 'font-semibold']">
-                                        {{ person.label }}
-                                    </span>
-
-                                    <span v-if="selected"
-                                        :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']">
-                                        <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                                    </span>
-                                </li>
-                            </ComboboxOption>
-                        </ComboboxOptions>
-                    </div>
-                </Combobox>
-            </div>
-        </div>
-        <button type="button"
-            class="rounded-full bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            data-te-toggle="tooltip" title="Add Permission">
-            <PlusIcon class="h-5 w-5" aria-hidden="true" />
-        </button>
+        <AccessEntry />
     </div>
     <button @click="submitForm()"
         class="rounded-md bg-white/10 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Submit</button>
---
+++ src/components/partials/AccessEntry.vue
@@ -0,0 +1,238 @@
+import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import {
+    Listbox,
+    ListboxButton,
+    ListboxLabel,
+    ListboxOption,
+    ListboxOptions,
+    Combobox,
+    ComboboxButton,
+    ComboboxInput,
+    ComboboxOption,
+    ComboboxOptions
+} from '@headlessui/vue'
+import {
+    PlusIcon,
+    CheckIcon,
+    ChevronDownIcon
+} from '@heroicons/vue/20/solid';
+import { watchDebounced } from '@vueuse/core';
+import { CompleteCharNamesRequest, CompleteJobNamesRequest } from '@arpanet/gen/services/completor/completor_pb';
+import { getCompletorClient, handleGRPCError } from '../../grpc';
+import { RpcError } from 'grpc-web';
+
+export default defineComponent({
+    components: {
+        PlusIcon,
+        CheckIcon,
+        ChevronDownIcon,
+        Listbox,
+        ListboxButton,
+        ListboxLabel,
+        ListboxOption,
+        ListboxOptions,
+        Combobox,
+        ComboboxButton,
+        ComboboxInput,
+        ComboboxOption,
+        ComboboxOptions,
+    },
+    data() {
+        return {
+            accessTypes: [
+                { id: 0, name: 'Citizen' },
+                { id: 1, name: 'Jobs' },
+            ],
+            selectedAccessType: ref<null | { id: number, name: string }>(null),
+            entriesName: [] as { id: string | number, label: string }[],
+            queryName: { value: '' },
+            selectedName: ref(null),
+            entriesAccessRole: [] as { id: string | number, label: string }[],
+            queryAccessRole: { value: '' },
+            selectedAccessRole: ref(null),
+            entriesMinimumRank: [] as { id: string | number, label: string }[],
+            queryMinimumRank: { value: '' },
+            selectedMinimumRank: ref(null),
+        }
+    },
+    mounted() {
+        this.selectedAccessType = this.accessTypes[1];
+
+        watchDebounced(this.queryName, () => {
+            if (this.selectedAccessType?.id === 0) {
+                this.findChars();
+            } else {
+                this.findJobs();
+            }
+        }, { debounce: 750, maxWait: 2000 });
+    },
+    methods: {
+        findJobs(): void {
+            const req = new CompleteJobNamesRequest();
+            req.setSearch(this.queryName.value);
+
+            getCompletorClient().completeJobNames(req, null).then((resp) => {
+                this.entriesName = [];
+                resp.getJobsList().forEach((job) => {
+                    this.entriesName.push({ id: job.getName(), label: job.getLabel() })
+                })
+            }).catch((err: RpcError) => {
+                handleGRPCError(err, this.$route);
+            })
+        },
+        findChars(): void {
+            const req = new CompleteCharNamesRequest();
+            req.setSearch(this.queryName.value);
+
+            getCompletorClient().completeCharNames(req, null).then((resp) => {
+                this.entriesName = [];
+                resp.getUsersList().forEach((user) => {
+                    this.entriesName.push({ id: user.getUserId(), label: `${user.getFirstname()} ${user.getLastname()}` })
+                })
+            }).catch((err: RpcError) => {
+                handleGRPCError(err, this.$route);
+            })
+        },
+    }
+})
+
+</script>
+
+<template>
+    <div class="flex flex-row">
+        <div class="flex-initial w-60 mr-2">
+            <Listbox as="div" v-model="selectedAccessType">
+                <div class="relative mt-2">
+                    <ListboxButton
+                        class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <span class="block truncate">{{ selectedAccessType?.name }}</span>
+                        <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <ChevronDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                        </span>
+                    </ListboxButton>
+
+                    <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+                        leave-to-class="opacity-0">
+                        <ListboxOptions
+                            class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                            <ListboxOption as="template" v-for="accessType in accessTypes" :key="accessType.id"
+                                :value="accessType" v-slot="{ active, selected }">
+                                <li
+                                    :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-8 pr-4']">
+                                    <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">{{
+                                        accessType.name
+                                    }}</span>
+
+                                    <span v-if="selected"
+                                        :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 left-0 flex items-center pl-1.5']">
+                                        <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                    </span>
+                                </li>
+                            </ListboxOption>
+                        </ListboxOptions>
+                    </transition>
+                </div>
+            </Listbox>
+        </div>
+        <div class="flex-1 mr-2">
+            <Combobox as="div" v-model="selectedName">
+                <div class="relative mt-2">
+                    <ComboboxButton as="div">
+                        <ComboboxInput
+                            class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            @change="queryName.value = $event.target.value"
+                            @click="selectedAccessType?.id === 0 ? findChars() : findJobs()"
+                            :display-value="(entry: any) => entry?.label" />
+                    </ComboboxButton>
+
+                    <ComboboxOptions v-if="entriesName.length > 0"
+                        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <ComboboxOption v-for="entry in entriesName" :key="entry.id" :value="entry" as="template"
+                            v-slot="{ active, selected }">
+                            <li
+                                :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
+                                <span :class="['block truncate', selected && 'font-semibold']">
+                                    {{ entry.label }}
+                                </span>
+
+                                <span v-if="selected"
+                                    :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']">
+                                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                            </li>
+                        </ComboboxOption>
+                    </ComboboxOptions>
+                </div>
+            </Combobox>
+        </div>
+        <div class="flex-1 mr-2" :hidden="selectedAccessType?.id === 0">
+            <Combobox as="div" v-model="selectedMinimumRank">
+                <div class="relative mt-2">
+                    <ComboboxButton as="div">
+                        <ComboboxInput
+                            class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            @change="queryMinimumRank.value = $event.target.value"
+                            @click="selectedAccessType?.id === 0 ? findChars() : findJobs()"
+                            :display-value="(person: any) => person?.label" />
+                    </ComboboxButton>
+
+                    <ComboboxOptions v-if="entriesMinimumRank.length > 0"
+                        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <ComboboxOption v-for="entry in entriesMinimumRank" :key="entry.id" :value="entry" as="template"
+                            v-slot="{ active, selected }">
+                            <li
+                                :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
+                                <span :class="['block truncate', selected && 'font-semibold']">
+                                    {{ entry.label }}
+                                </span>
+
+                                <span v-if="selected"
+                                    :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']">
+                                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                            </li>
+                        </ComboboxOption>
+                    </ComboboxOptions>
+                </div>
+            </Combobox>
+        </div>
+        <div class="flex-1">
+            <Combobox as="div" v-model="selectedAccessRole">
+                <div class="relative mt-2">
+                    <ComboboxButton as="div">
+                        <ComboboxInput
+                            class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            @change="queryName.value = $event.target.value"
+                            @click="selectedAccessType?.id === 0 ? findChars() : findJobs()"
+                            :display-value="(entry: any) => entry?.label" />
+                    </ComboboxButton>
+
+                    <ComboboxOptions v-if="entriesAccessRole.length > 0"
+                        class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        <ComboboxOption v-for="entry in entriesAccessRole" :key="entry.id" :value="entry" as="template"
+                            v-slot="{ active, selected }">
+                            <li
+                                :class="['relative cursor-default select-none py-2 pl-8 pr-4', active ? 'bg-indigo-600 text-white' : 'text-gray-900']">
+                                <span :class="['block truncate', selected && 'font-semibold']">
+                                    {{ entry.label }}
+                                </span>
+
+                                <span v-if="selected"
+                                    :class="['absolute inset-y-0 left-0 flex items-center pl-1.5', active ? 'text-white' : 'text-indigo-600']">
+                                    <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                </span>
+                            </li>
+                        </ComboboxOption>
+                    </ComboboxOptions>
+                </div>
+            </Combobox>
+        </div>
+    </div>
+    <button type="button"
+        class="rounded-full bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        data-te-toggle="tooltip" title="Add Permission">
+        <PlusIcon class="h-5 w-5" aria-hidden="true" />
+    </button>
+</template>
