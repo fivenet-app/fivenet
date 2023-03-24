95cb7789f3a3acc770f0704a7ba29985d20563f0
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-24 22:08:53 +0100

    Finished DocumentsList

--- src/components/documents/DocumentsList.vue
+++ src/components/documents/DocumentsList.vue
@@ -54,24 +54,24 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <div class="py-2">
+    <div class="py-2 sm:min-h-[63rem]">
         <div class="px-2 sm:px-6 lg:px-8">
             <div class="sm:flex sm:items-center">
                 <div class="sm:flex-auto">
                     <form @submit.prevent="findDocuments(0)">
-                        <div class="grid grid-cols-5 gap-4">
-                            <div class="col-span-4 form-control">
-                                <label for="search" class="block text-sm font-medium leading-6 text-neutral">Title Search</label>
-                                <div class="relative mt-2 flex items-center">
-                                    <input v-model="search.title" ref="searchInput"
-                                        type="text" name="search" id="search"
-                                        class="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <label for="search" class="block text-sm font-medium leading-6 text-neutral mb-2">Search</label>
+                        <div class="flex flex-row gap-4 mx-auto items-center">
+                            <div class="flex-1 form-control">
+                                <div class="relative">
+                                    <input v-model="search.title" ref="searchInput" type="text" name="search" id="search"
+                                        placeholder="Title"
+                                        class="block w-full rounded-md border-0 py-1.5 pr-14 bg-base-700 text-neutral shadow-sm placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6" />
                                 </div>
                             </div>
-                            <div class="form-control">
-                                <div class="relative mt-2 flex items-center">
+                            <div class="flex-initial form-control">
+                                <div class="relative">
                                     <router-link :to="{ name: 'Documents: Create' }"
-                                        class="rounded-md bg-white/10 py-2.5 px-3.5 text-sm font-semibold text-neutral shadow-sm hover:bg-white/20">Create</router-link>
+                                        class="inline-flex rounded-md bg-primary-500 px-3 py-2 text-sm font-semibold text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">Create</router-link>
                                 </div>
                             </div>
                         </div>
@@ -89,47 +89,42 @@ onBeforeMount(() => {
                                 query or create the first document using the above "Create"-button.</span>
                         </button>
                         <div v-else>
-                            <ul role="list" class="divide-y divide-gray-200">
-                                <li v-for="doc in documents" :key="doc.getId()">
-                                    <router-link :to="{ name: 'Documents: Info', params: { id: doc.getId() } }"
-                                        class="block hover:bg-gray-50">
-                                        <div class="px-4 py-4 sm:px-6">
-                                            <div class="flex items-center justify-between">
-                                                <p class="truncate text-sm font-medium text-indigo-600">{{ doc.getTitle() }}
+                            <ul class="flex flex-col">
+                                <li v-for="doc in documents" :key="doc.getId()" class="flex-initial hover:bg-base-800 rounded-lg">
+                                    <router-link :to="{ name: 'Documents: Info', params: { id: doc.getId() } }">
+                                        <div class="mx-2 mt-1 mb-4">
+                                            <div class="flex flex-row">
+                                            <p class="py-2 pl-4 pr-3 text-sm font-medium text-neutral sm:pl-0">
+                                                {{ doc.getTitle() }}
+                                            </p>
+                                            <p class="px-2 py-2 text-sm text-neutral ml-auto">
+                                            <p
+                                                class="inline-flex rounded-full bg-primary-100 px-2 text-xs font-semibold leading-5 text-primary-700">
+                                                {{ doc.getState() }}</p>
+                                            </p>
+                                        </div>
+                                        <div class="flex flex-row gap-2 text-base-200">
+                                            <div class="flex flex-1 justify-start flex-row items-center">
+                                                <UserIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-base-400"
+                                                    aria-hidden="true" />
+                                                {{ doc.getCreator()?.getFirstname() }}, {{
+                                                    doc.getCreator()?.getLastname()
+                                                }}
+                                            </div>
+                                            <div class="flex flex-1 justify-center flex-row items-center">
+                                                <BriefcaseIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-base-400"
+                                                    aria-hidden="true" />
+                                                {{ doc.getCreator()?.getJobLabel() }}
+                                            </div>
+                                            <div class="flex flex-1 justify-end flex-row items-center">
+                                                <CalendarIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-base-400"
+                                                    aria-hidden="true" />
+                                                <p>
+                                                    Created <time :datetime="getDateLocaleString(doc.getCreatedAt())">{{
+                                                        getDateRelativeString(doc.getCreatedAt()) }}</time>
                                                 </p>
-                                                <div class="ml-2 flex flex-shrink-0">
-                                                    <p
-                                                        class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                                                        {{ doc.getState() }}</p>
-                                                </div>
                                             </div>
-                                            <div class="mt-2 sm:flex sm:justify-between">
-                                                <div class="sm:flex">
-                                                    <p class="max-w-2xl truncate text-gray-300">{{ doc.getContent() }}</p>
-                                                </div>
-                                                <div class="sm:flex">
-                                                    <p class="flex items-center text-sm text-gray-300">
-                                                        <UserIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                                                            aria-hidden="true" />
-                                                        {{ doc.getCreator()?.getFirstname() }}, {{
-                                                            doc.getCreator()?.getLastname()
-                                                        }}
-                                                    </p>
-                                                    <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
-                                                        <BriefcaseIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                                                            aria-hidden="true" />
-                                                        {{ doc.getCreator()?.getJobLabel() }}
-                                                    </p>
-                                                </div>
-                                                <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                                                    <CalendarIcon class="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
-                                                        aria-hidden="true" />
-                                                    <p>
-                                                        Created <time :datetime="getDateLocaleString(doc.getCreatedAt())">{{
-                                                            getDateRelativeString(doc.getCreatedAt()) }}</time>
-                                                    </p>
-                                                </div>
-                                            </div>
+                                        </div>
                                         </div>
                                     </router-link>
                                 </li>
--- src/components/partials/TablePagination.vue
+++ src/components/partials/TablePagination.vue
@@ -45,10 +45,10 @@ defineProps({
         <div class="flex flex-1 justify-between sm:justify-end">
             <button :class="[offset <= 0 ? 'disabled' : '']" :disabled="offset <= 0" v-on:click="callback(offset - entries)"
                 type="button"
-                class="relative inline-flex items-center rounded-md bg-primary-500 px-3 py-2 text-sm font-semibold text-neutral shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Previous</button>
+                class="relative inline-flex items-center rounded-md bg-primary-500 px-3 py-2 text-sm font-semibold text-neutral shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer">Previous</button>
             <button :class="[offset >= total ? 'disabled' : '']" :disabled="(offset + end) >= total"
                 v-on:click="callback(end)" type="button"
-                class="relative ml-3 inline-flex items-center rounded-md bg-primary-500 px-3 py-2 text-sm font-semibold text-neutral shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Next</button>
+                class="relative ml-3 inline-flex items-center rounded-md bg-primary-500 px-3 py-2 text-sm font-semibold text-neutral shadow-sm hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500 cursor-pointer">Next</button>
         </div>
     </nav>
 </template>
