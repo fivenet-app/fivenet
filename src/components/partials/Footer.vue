cfcb3ff0019d54c738dfa6f10cbe68edfa93a8b3
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-24 16:44:14 +0100

    Make everything prettier

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,13 +1,7 @@
-<script lang="ts" setup>
-const navigation = {
-    logo: '/images/logo.png',
-};
-</script>
-
 <template>
     <footer class="bg-base-850">
         <div class="py-6 sm:py-8 flex flex-row items-center mx-auto w-fit gap-5">
-            <img class="w-16 h-auto" :src="navigation.logo" alt="CIP Logo" />
+            <img class="w-16 h-auto" src="/images/logo.png" alt="CIP Logo" />
             <p class=" text-center text-s leading-5 text-neutral">
                 Copyright &copy; 2023 Galexrt All rights reserved.
             </p>
