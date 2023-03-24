e94611cfa093931598b38f53a7a7151f20e4f51a
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-24 13:23:32 +0100

    Changed Cancel to Close Buttons

--- src/components/documents/DocumentReferenceManager.vue
+++ src/components/documents/DocumentReferenceManager.vue
@@ -147,7 +147,7 @@ function removeReference(id: number): void {
                             <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse gap-2">
                                 <button type="button"
                                     class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                    @click="emit('close')">Cancel</button>
+                                    @click="emit('close')">Close</button>
                             </div>
                         </DialogPanel>
                     </TransitionChild>
