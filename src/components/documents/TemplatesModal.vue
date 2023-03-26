173460e559e6c288ed8a975903ebfbb377cd8108
Alexander Trost <galexrt@googlemail.com>
2023-03-27 00:36:41 +0200

    add no template button to templatesmodal
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/documents/TemplatesModal.vue
+++ src/components/documents/TemplatesModal.vue
@@ -58,6 +58,10 @@ function templateSelected(t: DocumentTemplateShort) {
                                             Document Templates
                                         </DialogTitle>
                                         <div class="mt-2 text-white">
+                                            <router-link :to="{ name: 'Documents: Create' }" type="button"
+                                                class="mb-5 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0">
+                                                No Template
+                                            </router-link>
                                             <TemplatesList @selected="(t: DocumentTemplateShort) => templateSelected(t)" />
                                         </div>
                                     </div>
