93f2879f15c54b19e1df4c393ceb0366df65c52c
Alexander Trost <galexrt@googlemail.com>
2023-03-27 18:55:06 +0200

    add clipboard button to document editor
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/pages/documents/create.vue
+++ src/pages/documents/create.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import ContentWrapper from '../../components/partials/ContentWrapper.vue';
 import DocumentEditor from '../../components/documents/DocumentEditor.vue';
+import ClipboardButton from '../../components/clipboard/ClipboardButton.vue';
 </script>
 
 <route lang="json">
@@ -16,5 +17,6 @@ import DocumentEditor from '../../components/documents/DocumentEditor.vue';
 <template>
     <ContentWrapper>
         <DocumentEditor />
+        <ClipboardButton />
     </ContentWrapper>
 </template>
