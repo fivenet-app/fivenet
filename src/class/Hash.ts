a03076c36318dd2491c36fd66147c87b4be8fcd0
Clover <39840298+SmollClover@users.noreply.github.com>
2023-03-31 20:24:29 +0200

    Make most variables in Hash const

--- src/class/Hash.ts
+++ src/class/Hash.ts
@@ -3,138 +3,138 @@ import { DomEvent, LatLng, Util } from 'leaflet';
 import { Livemap } from './Livemap';
 
 export interface parsedHash {
-	center: LatLng;
-	zoom: number;
+    center: LatLng;
+    zoom: number;
 }
 
 export class Hash {
-	private HAS_HASHCHANGE: boolean = 'onhashchange' in window;
+    private HAS_HASHCHANGE: boolean = 'onhashchange' in window;
 
-	private map: Livemap | undefined;
-	private app: HTMLElement | null;
+    private map: Livemap | undefined;
+    private app: HTMLElement | null;
 
-	private lastHash: string = '';
-	private changeDefer: number = 100;
-	private changeTimeout: NodeJS.Timeout | undefined = undefined;
-	private movingMap: boolean = false;
-	private isListening: boolean = false;
-	private hashChangeInterval: NodeJS.Timer | undefined = undefined;
+    private lastHash: string = '';
+    private changeDefer: number = 100;
+    private changeTimeout: NodeJS.Timeout | undefined = undefined;
+    private movingMap: boolean = false;
+    private isListening: boolean = false;
+    private hashChangeInterval: NodeJS.Timer | undefined = undefined;
 
-	constructor(map: Livemap, element: HTMLElement) {
-		this.onHashChange = Util.bind(this.onHashChange, this);
-		this.app = element;
-		this.map = map;
+    constructor(map: Livemap, element: HTMLElement) {
+        this.onHashChange = Util.bind(this.onHashChange, this);
+        this.app = element;
+        this.map = map;
 
-		this.onHashChange();
-		if (!this.isListening) this.startListening();
-	}
+        this.onHashChange();
+        if (!this.isListening) this.startListening();
+    }
 
-	public remove(): void {
-		if (this.changeTimeout) clearTimeout(this.changeTimeout);
-		if (this.isListening) this.stopListening();
+    public remove(): void {
+        if (this.changeTimeout) clearTimeout(this.changeTimeout);
+        if (this.isListening) this.stopListening();
 
-		this.map = undefined;
-	}
+        this.map = undefined;
+    }
 
-	private parseHash(hash: string): parsedHash | false {
-		if (hash.indexOf('#') === 0) {
-			hash = hash.substring(1);
-		}
-		var args = hash.split('/');
-		if (args.length == 3) {
-			var zoom = parseInt(args[0], 10),
-				lat = parseFloat(args[1]),
-				lon = parseFloat(args[2]);
-			if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
-				return false;
-			} else {
-				return {
-					center: new LatLng(lat, lon),
-					zoom: zoom,
-				};
-			}
-		} else {
-			return false;
-		}
-	}
+    private parseHash(hash: string): parsedHash | false {
+        if (hash.indexOf('#') === 0)  hash = hash.substring(1);
 
-	private formatHash(): string {
-		if (!this.map) throw new Error('map is undefined');
+        const args = hash.split('/');
+        if (args.length == 3) {
+            const zoom = parseInt(args[0], 10),
+                lat = parseFloat(args[1]),
+                lon = parseFloat(args[2]);
+            if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
+                return false;
+            } else {
+                return {
+                    center: new LatLng(lat, lon),
+                    zoom: zoom,
+                };
+            }
+        } else {
+            return false;
+        }
+    }
 
-		var center = this.map.getCenter(),
-			zoom = this.map.getZoom(),
-			precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+    private formatHash(): string {
+        if (!this.map) throw new Error('map is undefined');
 
-		return '#' + [zoom, center.lat.toFixed(precision), center.lng.toFixed(precision)].join('/');
-	}
+        const center = this.map.getCenter();
+        const zoom = this.map.getZoom();
+        const precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
 
-	private onMapMove(): void {
-		if (!this.map) return;
+        const hash = '#' + [zoom, center.lat.toFixed(precision), center.lng.toFixed(precision)].join('/');
+        return hash;
+    }
 
-		if (this.movingMap || !this.map.hasLoaded) return;
+    private onMapMove(): void {
+        if (!this.map) return;
 
-		var hash = this.formatHash();
-		if (this.lastHash != hash) {
-			window.location.replace(hash);
-			this.lastHash = hash;
-		}
-	}
+        if (this.movingMap || !this.map.hasLoaded) return;
 
-	private update(): void {
-		if (!this.map) return;
+        const hash = this.formatHash();
+        if (this.lastHash != hash) {
+            window.location.replace(hash);
+            this.lastHash = hash;
+        }
+    }
 
-		var hash = window.location.hash;
-		if (hash === this.lastHash) {
-			return;
-		}
-		var parsed = this.parseHash(hash);
-		if (parsed) {
-			this.movingMap = true;
+    private update(): void {
+        if (!this.map) return;
 
-			this.map.setView(parsed.center, parsed.zoom);
+        const hash = window.location.hash;
+        if (hash === this.lastHash) {
+            return;
+        }
+        const parsed = this.parseHash(hash);
+        if (parsed) {
+            this.movingMap = true;
 
-			this.movingMap = false;
-		} else {
-			this.onMapMove();
-		}
-	}
+            this.map.setView(parsed.center, parsed.zoom);
 
-	private onHashChange(): void {
-		if (!this.changeTimeout) {
-			var that = this;
-			this.changeTimeout = setTimeout(() => {
-				that.update();
-				that.changeTimeout = undefined;
-			}, this.changeDefer);
-		}
-	}
+            this.movingMap = false;
+        } else {
+            this.onMapMove();
+        }
+    }
 
-	private startListening(): void {
-		if (!this.map) return;
+    private onHashChange(): void {
+        if (!this.changeTimeout) {
+            const that = this;
+            this.changeTimeout = setTimeout(() => {
+                that.update();
+                that.changeTimeout = undefined;
+            }, this.changeDefer);
+        }
+    }
 
-		this.map.on('moveend', this.onMapMove, this);
+    private startListening(): void {
+        if (!this.map) return;
 
-		if (this.HAS_HASHCHANGE) {
-			DomEvent.addListener(this.app as HTMLElement, 'hashchange', this.onHashChange);
-		} else {
-			clearInterval(this.hashChangeInterval);
-			this.hashChangeInterval = setInterval(this.onHashChange, 50);
-		}
+        this.map.on('moveend', this.onMapMove, this);
 
-		this.isListening = true;
-	}
+        if (this.HAS_HASHCHANGE) {
+            DomEvent.addListener(this.app as HTMLElement, 'hashchange', this.onHashChange);
+        } else {
+            clearInterval(this.hashChangeInterval);
+            this.hashChangeInterval = setInterval(this.onHashChange, 50);
+        }
 
-	private stopListening(): void {
-		if (!this.map) return;
+        this.isListening = true;
+    }
 
-		this.map.off('moveend', this.onMapMove, this);
+    private stopListening(): void {
+        if (!this.map) return;
 
-		if (this.HAS_HASHCHANGE) {
-			DomEvent.removeListener(this.app as HTMLElement, 'hashchange', this.onHashChange);
-		} else {
-			clearInterval(this.hashChangeInterval);
-		}
+        this.map.off('moveend', this.onMapMove, this);
 
-		this.isListening = false;
-	}
+        if (this.HAS_HASHCHANGE) {
+            DomEvent.removeListener(this.app as HTMLElement, 'hashchange', this.onHashChange);
+        } else {
+            clearInterval(this.hashChangeInterval);
+        }
+
+        this.isListening = false;
+    }
 }
