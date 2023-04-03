c179f04763e76fbe69449d622637e14bbe7aa28d
Alexander Trost <galexrt@googlemail.com>
2023-04-03 19:42:44 +0200

    add perms editing to role view
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/rector/RoleView.vue
+++ src/components/rector/RoleView.vue
@@ -10,6 +10,7 @@ import Divider from '../partials/Divider.vue';
 import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/vue';
 import { CheckIcon } from '@heroicons/vue/20/solid';
 import { watchDebounced } from '@vueuse/shared';
+import { dispatchNotification } from '../notification';
 
 const { $grpc } = useNuxtApp();
 
@@ -56,7 +57,6 @@ async function deleteRole(): Promise<void> {
 
 const entriesPerms = ref<Array<Permission>>([]);
 const queryPerm = ref('');
-const selectedPerm = ref();
 
 async function getPermissions(): Promise<Array<Permission>> {
     return new Promise(async (res, rej) => {
@@ -75,30 +75,57 @@ async function getPermissions(): Promise<Array<Permission>> {
     });
 }
 
+const selectedPerm = ref<undefined | Permission>(undefined);
 const permsToAdd = ref<Array<Permission>>([]);
 const permsToRemove = ref<Array<Permission>>([]);
 
 function addPermission(): void {
+    if (!selectedPerm.value) {
+        return;
+    }
+
+    // Remove perm from "to be removed" list
+    const idx = permsToRemove.value.indexOf(selectedPerm.value);
+    if (idx > -1) {
+        permsToRemove.value.splice(idx, 1);
+    }
+
     if (permsToAdd.value.indexOf(selectedPerm.value) === -1) {
         permsToAdd.value.push(selectedPerm.value);
     }
-    selectedPerm.value = null;
+
+    const pIdx = role.value?.getPermissionsList().indexOf(selectedPerm.value) ?? -1;
+    if (pIdx === -1) {
+        role.value?.getPermissionsList().push(selectedPerm.value);
+    }
+
+    console.log(role.value?.getPermissionsList());
 }
 
 function removePermission(perm: Permission): void {
-    const addIdx = permsToAdd.value.indexOf(perm);
-    if (addIdx > -1) {
-        permsToAdd.value.splice(addIdx, 1);
+    // Remove perm from "to be added" list
+    const idx = permsToAdd.value.indexOf(perm);
+    if (idx > -1) {
+        permsToAdd.value.splice(idx, 1);
     }
 
-    permsToRemove.value.push(perm);
-}
+    if (permsToRemove.value.indexOf(perm) === -1) {
+        permsToRemove.value.push(perm);
+    }
 
-async function saveRolePermissions(): Promise<void> {
-    await Promise.all([saveAddPermissions(), saveRemovePermissions()]);
+    const pIdx = role.value?.getPermissionsList().indexOf(perm) ?? -1;
+    if (pIdx > -1) {
+        role.value?.getPermissionsList().splice(pIdx, 1);
+    }
+
+    console.log(role.value?.getPermissionsList());
 }
 
 async function saveAddPermissions(): Promise<void> {
+    if (permsToAdd.value.length === 0) {
+        return;
+    }
+
     return new Promise(async (res, rej) => {
         const req = new AddPermToRoleRequest();
         req.setId(props.roleId);
@@ -108,7 +135,7 @@ async function saveAddPermissions(): Promise<void> {
 
         try {
             return $grpc.getRectorClient().
-                removePermFromRole(req, null);
+                addPermToRole(req, null);
         } catch (e) {
             $grpc.handleRPCError(e as RpcError);
             return rej(e as RpcError);
@@ -117,6 +144,10 @@ async function saveAddPermissions(): Promise<void> {
 }
 
 async function saveRemovePermissions(): Promise<void> {
+    if (permsToRemove.value.length === 0) {
+        return;
+    }
+
     return new Promise(async (res, rej) => {
         const req = new RemovePermFromRoleRequest();
         req.setId(props.roleId);
@@ -132,6 +163,14 @@ async function saveRemovePermissions(): Promise<void> {
             return rej(e as RpcError);
         }
     });
+}
+
+async function saveRolePermissions(): Promise<void> {
+    console.log("saveRolePermissions");
+    console.log(permsToAdd.value);
+    console.log(permsToRemove.value);
+    await Promise.all([saveAddPermissions(), saveRemovePermissions()]);
+    dispatchNotification({ title: 'Saving Role Permissions', content: '', type: 'success' });
 }
 
 onMounted(async () => {
@@ -166,6 +205,7 @@ watchDebounced(queryPerm, async () => await getPermissions(), { debounce: 750, m
                                             <ComboboxButton as="div">
                                                 <ComboboxInput
                                                     class="block w-full rounded-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6"
+                                                    @change="queryPerm = $event.target.value"
                                                     :display-value="(perm: any) => perm ? `${perm?.getName()}: ${perm?.getDescription()}` : ''"
                                                     placeholder="Permission" />
                                             </ComboboxButton>
@@ -192,13 +232,14 @@ watchDebounced(queryPerm, async () => await getPermissions(), { debounce: 750, m
                                 </div>
                             </div>
                             <div class="flex-initial form-control">
-                                <button @click="addPermission()" :disabled="!queryPerm"
+                                <button type="submit" :disabled="!selectedPerm"
                                     class="inline-flex px-3 py-2 text-sm font-semibold rounded-md bg-primary-500 text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">
                                     Add Permission
                                 </button>
                             </div>
                             <div class="flex-initial form-control">
-                                <button @click="saveRolePermissions()" :disabled="permsToAdd.length === 0 && permsToRemove.length === 0"
+                                <button @click="saveRolePermissions()"
+                                    :disabled="permsToAdd.length === 0 && permsToRemove.length === 0"
                                     class="inline-flex px-3 py-2 text-sm font-semibold rounded-md bg-success-700 text-neutral hover:bg-success-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-success-700">
                                     Save Changes
                                 </button>
