d671dcef5f1bdaa9f98aab0b92c38d7fc3626ea7
Alexander Trost <galexrt@googlemail.com>
2023-04-10 23:42:33 +0200

    add basic audit log view
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- proto/services/rector/rector_audit.go
+++ proto/services/rector/rector_audit.go
@@ -1,11 +1,59 @@
 package rector
 
-import "context"
+import (
+	"context"
+
+	database "github.com/galexrt/fivenet/proto/resources/common/database"
+	"github.com/galexrt/fivenet/query/fivenet/table"
+	jet "github.com/go-jet/jet/v2/mysql"
+)
+
+var (
+	audit = table.FivenetAuditLog
+)
 
 func (s *Server) ViewAuditLog(ctx context.Context, req *ViewAuditLogRequest) (*ViewAuditLogResponse, error) {
-	resp := &ViewAuditLogResponse{}
+	countStmt := audit.
+		SELECT(
+			jet.COUNT(audit.ID).AS("datacount.totalcount"),
+		).
+		FROM(
+			audit,
+		)
 
-	// TODO
+	var count database.DataCount
+	if err := countStmt.QueryContext(ctx, s.db, &count); err != nil {
+		return nil, err
+	}
+
+	resp := &ViewAuditLogResponse{
+		Pagination: database.EmptyPaginationResponse(req.Pagination.Offset),
+	}
+	if count.TotalCount <= 0 {
+		return resp, nil
+	}
+
+	stmt := audit.
+		SELECT(
+			audit.AllColumns,
+		).
+		FROM(
+			audit,
+		).
+		ORDER_BY(
+			audit.CreatedAt.ASC(),
+		).
+		OFFSET(req.Pagination.Offset).
+		LIMIT(database.DefaultPageLimit)
+
+	if err := stmt.QueryContext(ctx, s.db, &resp.Logs); err != nil {
+		return nil, err
+	}
+
+	database.PaginationHelper(resp.Pagination,
+		count.TotalCount,
+		req.Pagination.Offset,
+		len(resp.Logs))
 
 	return resp, nil
 }
