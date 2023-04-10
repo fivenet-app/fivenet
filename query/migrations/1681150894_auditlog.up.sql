446d64ee0fdb7151b4ca0d8b4e3c1a90b09dc9d3
Alexander Trost <galexrt@googlemail.com>
2023-04-10 20:34:11 +0200

    add audit log tables
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ query/migrations/1681150894_auditlog.up.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+-- Table: fivenet_audit_log
+CREATE TABLE IF NOT EXISTS `fivenet_audit_log` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  `service` varchar(255) NOT NULL,
+  `method` varchar(255) NOT NULL,
+  `state` smallint(2) NOT NULL,
+  `data` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_fivenet_audit_log_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+COMMIT;
