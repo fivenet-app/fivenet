3143c2c8a0eacd8a74ffdcb283859df14d6090ee
Clover <39840298+SmollClover@users.noreply.github.com>
2023-04-11 17:21:38 +0200

    Style NotificationItem

--- src/components/partials/notification/NotificationItem.vue
+++ src/components/partials/notification/NotificationItem.vue
@@ -19,26 +19,26 @@ const closeNotification = (id: string) => {
         enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
         enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition duration-100 ease-in"
         leave-from-class="opacity-100" leave-to-class="opacity-0">
-        <div class="z-50 w-full max-w-sm overflow-hidden bg-white rounded-lg pointer-events-auto shadow-float ring-1 ring-black ring-opacity-5">
+        <div class="z-50 w-full max-w-sm overflow-hidden bg-base-800 rounded-lg pointer-events-auto shadow-float text-neutral">
             <div class="p-4">
                 <div class="flex items-start">
-                    <div class="flex-shrink-0" v-if="notification.type">
-                        <CheckCircleIcon v-if="notification.type === 'success'" class="w-6 h-6 text-green-400" aria-hidden="true" />
-                        <InformationCircleIcon v-else-if="notification.type === 'info'" class="w-6 h-6 text-blue-400" aria-hidden="true" />
-                        <ExclamationTriangleIcon v-else-if="notification.type === 'warning'" class="w-6 h-6 text-yellow-400" aria-hidden="true" />
-                        <ExclamationCircleIcon v-else-if="notification.type === 'error'" class="w-6 h-6 text-red-400" aria-hidden="true" />
+                    <div class="flex-shrink-0 w-8 my-auto" v-if="notification.type">
+                        <CheckCircleIcon v-if="notification.type === 'success'" class="text-success-400" aria-hidden="true" />
+                        <InformationCircleIcon v-else-if="notification.type === 'info'" class="text-info-400" aria-hidden="true" />
+                        <ExclamationTriangleIcon v-else-if="notification.type === 'warning'" class="text-warn-400" aria-hidden="true" />
+                        <ExclamationCircleIcon v-else-if="notification.type === 'error'" class="text-error-400" aria-hidden="true" />
                     </div>
                     <div class="ml-3 w-0 flex-1 pt-0.5">
-                        <p class="text-sm font-medium text-gray-900" v-if="notification.title">
+                        <p class="text-sm font-semibold" v-if="notification.title">
                             {{ notification.title }}
                         </p>
-                        <p :class="`${notification.title ? 'mt-1' : ''} text-sm leading-5 text-gray-500`">
+                        <p :class="`${notification.title ? 'mt-1' : ''} text-sm leading-5`">
                             {{ notification.content }}
                         </p>
                     </div>
                     <div class="flex flex-shrink-0 ml-4">
                         <button @click="() => closeNotification(notification.id)" type="button"
-                            class="inline-flex text-gray-400 bg-white rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            class="inline-flex text-neutral hover:text-base-300 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2">
                             <span class="sr-only">Close</span>
                             <XMarkIcon class="w-5 h-5" aria-hidden="true" />
                         </button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,7 @@ module.exports = {
             },
             boxShadow: {
                 float: '0px 0px 22px 0px rgba(0,0,0,0.5)',
+                glow: '0px 0px 22px 0px rgba(255,255,255,0.2)',
             },
             colors: {
                 neutral: '#ffffff',
