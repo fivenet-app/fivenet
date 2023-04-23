627ae89f1a88816a38495b12e7a2212a44c0074f
Clover <39840298+SmollClover@users.noreply.github.com>
2023-04-23 20:49:43 +0200

    lang: 404 Page

--- src/pages/[...path].vue
+++ src/pages/[...path].vue
@@ -17,10 +17,11 @@ const accessToken = computed(() => store.$state.accessToken);
 
 <template>
     <HeroFull>
-        <h1 class="text-5xl font-bold text-neutral">Page Not Found</h1>
-        <h2 class="text-4xl text-neutral">4😵‍💫4 Error</h2>
-        <p class="py-6 text-neutral">Seems like you got lost. Let's get you back to the start!</p>
-        <NuxtLink v-if="accessToken" :to="{ name: 'overview' }" class="btn btn-primary">Overview</NuxtLink>
-        <NuxtLink v-else :to="{ name: 'auth-login' }" class="btn btn-primary">Login</NuxtLink>
+        <h1 class="text-5xl font-bold text-neutral">{{ $t('pages.404.page_not_found') }}</h1>
+        <h2 class="text-4xl text-neutral">{{ $t('pages.404.error') }}</h2>
+        <p class="py-6 text-neutral">{{ $t('pages.404.fun_error') }}</p>
+        <NuxtLink v-if="accessToken" :to="{ name: 'overview' }" class="btn btn-primary">{{ $t('common.overview') }}
+        </NuxtLink>
+        <NuxtLink v-else :to="{ name: 'auth-login' }" class="btn btn-primary">${{ $t('common.login') }}</NuxtLink>
     </HeroFull>
 </template>
