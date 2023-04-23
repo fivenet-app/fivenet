2a502d2afc5970363e2d9b7d2f23ad7866703b53
Clover <39840298+SmollClover@users.noreply.github.com>
2023-04-23 22:26:15 +0200

    lang: TablePagination

--- src/components/partials/TablePagination.vue
+++ src/components/partials/TablePagination.vue
@@ -19,29 +19,15 @@ const end = computed(() => props.pagination?.getEnd() ?? 0);
 <template>
     <nav class="flex items-center justify-between px-4 py-3 border-t sm:px-6" aria-label="Pagination">
         <div class="hidden sm:block">
-            <p class="text-sm text-gray-300">
-                Showing
-                {{ ' ' }}
-                <span class="font-medium text-neutral">{{ total == 0 ? offset : offset + 1 }}</span>
-                {{ ' ' }}
-                to
-                {{ ' ' }}
-                <span class="font-medium text-neutral">{{ end }}</span>
-                {{ ' ' }}
-                of
-                {{ ' ' }}
-                <span class="font-medium text-neutral">{{ total }}</span>
-                {{ ' ' }}
-                results
-            </p>
+            <p class="text-sm text-gray-300" v-html="$t('components.partials.table_pagination.showing_results', [total == 0 ? offset : offset + 1, end, total])" />
         </div>
         <div class="flex justify-between flex-1 sm:justify-end">
             <button :class="[offset <= 0 ? 'disabled' : '']" :disabled="offset <= 0"
                 v-on:click="$emit('offsetChange', offset - pageSize)" type="button"
-                class="relative inline-flex items-center px-3 py-2 text-sm font-semibold rounded-md cursor-pointer bg-primary-500 text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Previous</button>
+                class="relative inline-flex items-center px-3 py-2 text-sm font-semibold rounded-md cursor-pointer bg-primary-500 text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">{{ $t('common.previous') }}</button>
             <button :class="[offset >= total ? 'disabled' : '']" :disabled="(end + offset) >= total"
                 v-on:click="$emit('offsetChange', end)" type="button"
-                class="relative inline-flex items-center px-3 py-2 ml-3 text-sm font-semibold rounded-md cursor-pointer bg-primary-500 text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">Next</button>
+                class="relative inline-flex items-center px-3 py-2 ml-3 text-sm font-semibold rounded-md cursor-pointer bg-primary-500 text-neutral hover:bg-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">{{ $t('common.next') }}</button>
         </div>
     </nav>
 </template>
