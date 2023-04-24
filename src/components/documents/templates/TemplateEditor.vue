c89c8bf9d469d768717b9b875a4823b09070beaf
Clover <39840298+SmollClover@users.noreply.github.com>
2023-04-24 16:29:59 +0200

    lang: TemplateEditor

--- src/components/documents/templates/TemplateEditor.vue
+++ src/components/documents/templates/TemplateEditor.vue
@@ -121,7 +121,7 @@ const onSubmit = handleSubmit(async (values): Promise<void> => await createTempl
             </div>
             <button type="submit"
                 class="flex justify-center w-full px-3 py-2 text-sm font-semibold transition-colors rounded-md bg-primary-600 text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-base-300">
-                Create
+                {{ $t('common.create') }}
             </button>
         </form>
     </div>
