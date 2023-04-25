91aff2d81c07571220ff9f29027f1e005d63a2d7
Alexander Trost <galexrt@googlemail.com>
2023-04-25 11:45:39 +0200

    version: bump to v0.1.0
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,7 +34,7 @@ const accessToken = computed(() => store.$state.accessToken);
                             {{ $t('common.overview') }}
                         </NuxtLink>
                         <NuxtLink v-else :to="{ name: 'auth-login' }"
-                            class="rounded-md w-20 bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">
+                            class="rounded-md w-24 bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">
                             {{ $t('common.login') }}
                         </NuxtLink>
                     </div>
