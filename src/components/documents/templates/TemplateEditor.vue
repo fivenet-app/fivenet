29737a50aefbc190c2e7b63fdc464a17c4f0264b
Clover <39840298+SmollClover@users.noreply.github.com>
2023-05-03 18:26:17 +0200

    feat: TemplateSchemaEditor

--- src/components/documents/templates/TemplateEditor.vue
+++ src/components/documents/templates/TemplateEditor.vue
@@ -4,8 +4,9 @@ import { object, string } from 'yup';
 import { toTypedSchema } from '@vee-validate/yup';
 import { CreateTemplateRequest, GetTemplateRequest, UpdateTemplateRequest } from '@fivenet/gen/services/docstore/docstore_pb';
 import { RpcError } from 'grpc-web';
-import { DocumentTemplate, TemplateSchema } from '@fivenet/gen/resources/documents/templates_pb';
+import { DocumentTemplate, ObjectSpecs, TemplateRequirements, TemplateSchema } from '@fivenet/gen/resources/documents/templates_pb';
 import TemplateSchemaEditor from './TemplateSchemaEditor.vue';
+import { TemplateSchemaEditorValue } from './TemplateSchemaEditor.vue';
 
 const { $grpc } = useNuxtApp();
 
@@ -20,7 +21,25 @@ const title = ref<string>('');
 const description = ref<string>('');
 const contentTitle = ref<string>('');
 const content = ref<string>('');
-const schema = ref<TemplateSchema>(new TemplateSchema());
+const schema = ref<TemplateSchemaEditorValue>({
+    users: {
+        req: false,
+        min: 0,
+        max: 0,
+    },
+
+    documents: {
+        req: false,
+        min: 0,
+        max: 0,
+    },
+
+    vehicles: {
+        req: false,
+        min: 0,
+        max: 0,
+    },
+});
 
 async function createTemplate(): Promise<void> {
     if (props.templateId) return updateTemplate();
@@ -32,7 +51,16 @@ async function createTemplate(): Promise<void> {
         tpl.setDescription(description.value);
         tpl.setContentTitle(contentTitle.value);
         tpl.setContent(content.value);
-        tpl.setSchema(schema.value);
+
+        const tRequirements = new TemplateRequirements();
+        if (schema.value.users.req) tRequirements.setUsers((new ObjectSpecs).setRequired(schema.value.users.req).setMin(schema.value.users.min).setMax(schema.value.users.max));
+        if (schema.value.documents.req) tRequirements.setDocuments((new ObjectSpecs).setRequired(schema.value.documents.req).setMin(schema.value.documents.min).setMax(schema.value.documents.max));
+        if (schema.value.vehicles.req) tRequirements.setVehicles((new ObjectSpecs).setRequired(schema.value.vehicles.req).setMin(schema.value.vehicles.min).setMax(schema.value.vehicles.max));
+
+        const tSchema = new TemplateSchema();
+        tSchema.setRequirements(tRequirements);
+
+        tpl.setSchema(tSchema);
 
         req.setTemplate(tpl);
 
@@ -58,7 +86,16 @@ async function updateTemplate(): Promise<void> {
         tpl.setDescription(description.value);
         tpl.setContentTitle(contentTitle.value);
         tpl.setContent(content.value);
-        tpl.setSchema(schema.value);
+
+        const tRequirements = new TemplateRequirements();
+        if (schema.value.users.req) tRequirements.setUsers((new ObjectSpecs).setRequired(schema.value.users.req).setMin(schema.value.users.min).setMax(schema.value.users.max));
+        if (schema.value.documents.req) tRequirements.setDocuments((new ObjectSpecs).setRequired(schema.value.documents.req).setMin(schema.value.documents.min).setMax(schema.value.documents.max));
+        if (schema.value.vehicles.req) tRequirements.setVehicles((new ObjectSpecs).setRequired(schema.value.vehicles.req).setMin(schema.value.vehicles.min).setMax(schema.value.vehicles.max));
+
+        const tSchema = new TemplateSchema();
+        tSchema.setRequirements(tRequirements);
+
+        tpl.setSchema(tSchema);
 
         try {
             const resp = await $grpc.getDocStoreClient().
@@ -97,11 +134,22 @@ onMounted(async () => {
             const resp = (await $grpc.getDocStoreClient().getTemplate(req, null)).getTemplate();
             if (!resp) return;
 
-            title.value = resp.getTitle()
-            description.value = resp.getDescription()
-            contentTitle.value = resp.getContentTitle()
-            content.value = resp.getContent()
-            schema.value = resp.getSchema() ?? new TemplateSchema();
+            title.value = resp.getTitle();
+            description.value = resp.getDescription();
+            contentTitle.value = resp.getContentTitle();
+            content.value = resp.getContent();
+
+            schema.value.users.req = resp.getSchema()?.getRequirements()?.getUsers()?.getRequired() ?? false;
+            schema.value.users.min = resp.getSchema()?.getRequirements()?.getUsers()?.getMin() ?? 0;
+            schema.value.users.max = resp.getSchema()?.getRequirements()?.getUsers()?.getMax() ?? 0;
+
+            schema.value.documents.req = resp.getSchema()?.getRequirements()?.getDocuments()?.getRequired() ?? false;
+            schema.value.documents.min = resp.getSchema()?.getRequirements()?.getDocuments()?.getMin() ?? 0;
+            schema.value.documents.max = resp.getSchema()?.getRequirements()?.getDocuments()?.getMax() ?? 0;
+
+            schema.value.vehicles.req = resp.getSchema()?.getRequirements()?.getVehicles()?.getRequired() ?? false;
+            schema.value.vehicles.min = resp.getSchema()?.getRequirements()?.getVehicles()?.getMin() ?? 0;
+            schema.value.vehicles.max = resp.getSchema()?.getRequirements()?.getVehicles()?.getMax() ?? 0;
         } catch (e) {
             $grpc.handleRPCError(e as RpcError);
         }
@@ -140,12 +188,11 @@ onMounted(async () => {
                     v-model="content" />
                 <ErrorMessage name="content" as="p" class="mt-2 text-sm text-error-400" />
             </div>
-            <label for="schema" class="block font-medium text-sm mt-2">Schema</label>
             <div>
-                <TemplateSchemaEditor v-model="schema" />
+                <TemplateSchemaEditor v-model="schema" class="mt-2" />
             </div>
             <button type="submit"
-                class="flex justify-center w-full px-3 py-2 text-sm font-semibold transition-colors rounded-md bg-primary-600 text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-base-300 mt-2">
+                class="flex justify-center w-full px-3 py-2 text-sm font-semibold transition-colors rounded-md bg-primary-600 text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-base-300 mt-4">
                 {{ $t('common.create') }}
             </button>
         </form>
--- src/components/documents/templates/TemplateSchemaEditor.vue
+++ src/components/documents/templates/TemplateSchemaEditor.vue
@@ -1,15 +1,105 @@
 <script lang="ts" setup>
-import { TemplateSchema } from '@fivenet/gen/resources/documents/templates_pb';
+export interface TemplateSchemaEditorValue {
+    users: {
+        req: boolean;
+        min: number;
+        max: number;
+    };
+
+    documents: {
+        req: boolean;
+        min: number;
+        max: number;
+    };
+
+    vehicles: {
+        req: boolean;
+        min: number;
+        max: number;
+    };
+}
 
 const props = defineProps<{
-    modelValue: TemplateSchema,
+    modelValue: TemplateSchemaEditorValue,
 }>();
 
 const emit = defineEmits<{
-    (e: 'update:modelValue', payload: TemplateSchema): void,
+    (e: 'update:modelValue', payload: TemplateSchemaEditorValue): void,
 }>();
 </script>
 
 <template>
-    Test
+    <div class="flex flex-row flex-initial gap-3">
+        <div class="flex-1">
+            <label for="users" class="block text-sm font-medium leading-6 text-neutral">
+                {{ $t('common.citizen', 2) }}
+            </label>
+            <div class="relative flex items-center gap-1">
+                <input v-model="modelValue.users.req" type="checkbox" name="users" id="users" data-te-toggle="tooltip"
+                    class="h-8 w-8 rounded-md transition-colors bg-base-700 text-primary-600 hover:text-primary-500 hover:bg-base-600 focus:ring-2 focus:ring-inset focus:ring-base-300 border-0" />
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.users.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Min</span>
+                    <input v-model="modelValue.users.min" :disabled="!modelValue.users.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="users" id="users"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.users.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Max</span>
+                    <input v-model="modelValue.users.max" :disabled="!modelValue.users.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="users" id="users"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+            </div>
+        </div>
+        <div class="flex-1">
+            <label for="documents" class="block text-sm font-medium leading-6 text-neutral">
+                {{ $t('common.document', 2) }}
+            </label>
+            <div class="relative flex items-center gap-1">
+                <input v-model="modelValue.documents.req" type="checkbox" name="documents" id="documents"
+                    data-te-toggle="tooltip"
+                    class="h-8 w-8 rounded-md transition-colors bg-base-700 text-primary-600 hover:text-primary-500 hover:bg-base-600 focus:ring-2 focus:ring-inset focus:ring-base-300 border-0" />
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.documents.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Min</span>
+                    <input v-model="modelValue.documents.min" :disabled="!modelValue.documents.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="documents" id="documents"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.documents.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Max</span>
+                    <input v-model="modelValue.documents.max" :disabled="!modelValue.documents.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="documents" id="documents"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+            </div>
+        </div>
+        <div class="flex-1">
+            <label for="vehicles" class="block text-sm font-medium leading-6 text-neutral">
+                {{ $t('common.vehicle', 2) }}
+            </label>
+            <div class="relative flex items-center gap-1">
+                <input v-model="modelValue.vehicles.req" type="checkbox" name="vehicles" id="vehicles"
+                    data-te-toggle="tooltip"
+                    class="h-8 w-8 rounded-md transition-colors bg-base-700 text-primary-600 hover:text-primary-500 hover:bg-base-600 focus:ring-2 focus:ring-inset focus:ring-base-300 border-0" />
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.vehicles.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Min</span>
+                    <input v-model="modelValue.vehicles.min" :disabled="!modelValue.vehicles.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="vehicles" id="vehicles"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+                <div class="flex flex-row">
+                    <span
+                        :class="[modelValue.vehicles.req ? 'text-neutral' : 'text-base-400', 'transition-colors inline-flex h-9 items-center rounded-l-md border-0 px-3 bg-base-800 sm:text-sm']">Max</span>
+                    <input v-model="modelValue.vehicles.max" :disabled="!modelValue.vehicles.req" type="number" min="0"
+                        oninput="validity.valid||(value='');" name="vehicles" id="vehicles"
+                        class="block w-full rounded-r-md border-0 py-1.5 bg-base-700 text-neutral placeholder:text-base-200 focus:ring-2 focus:ring-inset focus:ring-base-300 sm:text-sm sm:leading-6 disabled:text-base-400 transition-colors" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
