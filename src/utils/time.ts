f532c32ecf9193d3231dda5486733df666c19bfb
Alexander Trost <galexrt@googlemail.com>
2023-05-03 17:40:44 +0200

    feat: translate fromSecondsToFormattedDuration output
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,27 +1,29 @@
 import * as resources_timestamp_timestamp_pb from '@fivenet/gen/resources/timestamp/timestamp_pb';
 
 export function fromSecondsToFormattedDuration(seconds: number): string {
-    var w = Math.floor(seconds / (7 * (3600 * 24)));
-    var d = Math.floor(seconds / (3600 * 24));
-    var h = Math.floor((seconds % (3600 * 24)) / 3600);
-    var m = Math.floor((seconds % 3600) / 60);
-    var s = Math.floor(seconds % 60);
+    const { t } = useI18n();
+
+    const w = Math.floor(seconds / (7 * (3600 * 24)));
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor((seconds % (3600 * 24)) / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
 
     const parts = new Array<string>();
     if (w > 0) {
-        parts.push(w + (w == 1 ? ' week' : ' weeks'));
+        parts.push(`${w} ${t(`common.time_ago.week`, w)}`);
     }
     if (d > 0) {
-        parts.push(d + (d == 1 ? ' day' : ' days'));
+        parts.push(`${d} ${t(`common.time_ago.day`, d)}`);
     }
     if (h > 0) {
-        parts.push(h + (h == 1 ? ' hour' : ' hours'));
+        parts.push(`${h} ${t(`common.time_ago.hour`, h)}`);
     }
     if (m > 0) {
-        parts.push(m + (m == 1 ? ' minute' : ' minutes'));
+        parts.push(`${m} ${t(`common.time_ago.minute`, m)}`);
     }
     if (s > 0) {
-        parts.push(s + (s == 1 ? ' second' : ' seconds'));
+        parts.push(`${s} ${t(`common.time_ago.second`, s)}`);
     }
     return parts.join(', ');
 }
