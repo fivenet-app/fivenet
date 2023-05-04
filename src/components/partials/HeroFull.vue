750c829cb5296843acf2e2753e60191199126db5
Alexander Trost <galexrt@googlemail.com>
2023-05-04 18:28:39 +0200

    feat: use different pattern background for herofull component
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/partials/HeroFull.vue
+++ src/components/partials/HeroFull.vue
@@ -2,8 +2,16 @@
 import '~/assets/css/herofull-pattern.css';
 </script>
 
+<style scoped>
+.hero-overlay {
+    background-color: rgba(0, 0, 0, 0.6);
+}
+</style>
+
 <template>
     <div class="isolate bg-base-900 hero h-full">
-        <slot />
+        <div class="hero-overlay h-full">
+            <slot />
+        </div>
     </div>
 </template>
