ff1094012e7a12347436f7498435fb4dea8a070f
Alexander Trost <galexrt@googlemail.com>
2023-05-04 14:24:16 +0200

    feat: overwrite nuxt default error page
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/partials/ContentCenterWrapper.vue
+++ src/components/partials/ContentCenterWrapper.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import ContentWrapper from './ContentWrapper.vue';
-</script>
-
 <template>
     <div class="flex overflow-hidden justify-center items-center align-middle h-full">
         <main class="w-full">
--- src/pages/[...path].vue
+++ src/pages/[...path].vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { useAuthStore } from '~/store/auth';
 import HeroFull from '~/components/partials/HeroFull.vue';
-import { computed } from 'vue';
+import ContentCenterWrapper from '~/components/partials/ContentCenterWrapper.vue';
 
 useHead({
     title: 'pages.notfound.title',
@@ -17,11 +17,25 @@ const accessToken = computed(() => authStore.getAccessToken);
 
 <template>
     <HeroFull>
-        <h1 class="text-5xl font-bold text-neutral">{{ $t('pages.notfound.page_not_found') }}</h1>
-        <h2 class="text-4xl text-neutral">{{ $t('pages.notfound.error') }}</h2>
-        <p class="py-6 text-neutral">{{ $t('pages.notfound.fun_error') }}</p>
-        <NuxtLink v-if="accessToken" :to="{ name: 'overview' }" class="btn btn-primary">{{ $t('common.overview') }}
-        </NuxtLink>
-        <NuxtLink v-else :to="{ name: 'auth-login' }" class="btn btn-primary">${{ $t('common.login') }}</NuxtLink>
+        <ContentCenterWrapper class="max-w-2xl mx-auto text-center">
+            <h1 class="text-5xl font-bold text-neutral">
+                {{ $t('pages.notfound.page_not_found') }}
+            </h1>
+            <h2 class="text-4xl text-neutral">
+                {{ $t('pages.notfound.error') }}
+            </h2>
+            <p class="py-6 text-neutral">
+                {{ $t('pages.notfound.fun_error') }}
+            </p>
+
+            <NuxtLink v-if="accessToken" :to="{ name: 'overview' }"
+                class="rounded-md w-60 bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">
+                {{ $t('common.overview') }}
+            </NuxtLink>
+            <NuxtLink v-else :to="{ name: 'auth-login' }"
+                class="rounded-md w-60 bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-neutral hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500">
+                {{ $t('common.login') }}
+            </NuxtLink>
+        </ContentCenterWrapper>
     </HeroFull>
 </template>
