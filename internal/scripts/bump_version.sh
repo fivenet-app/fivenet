a1e3c0a32cd9d5cab74fe477ccd4d8298099ee26
Alexander Trost <galexrt@googlemail.com>
2023-05-10 15:38:32 +0200

    version: bump to v0.3.0
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- internal/scripts/bump_version.sh
+++ internal/scripts/bump_version.sh
@@ -20,14 +20,14 @@ echo "v${VERSION}" > VERSION
 sed \
     --in-place \
     --regexp-extended \
-    --expression 's~"version": "[\d.]+",~"version": "'"${VERSION}"'",~' \
+    --expression 's~"version": "[0-9\.]+"~"version": "'"${VERSION}"'"~' \
         ./package.json \
         ./gen/js/package.json
 
 sed \
     --in-place \
     --regexp-extended \
-    --expression 's~appVersion: "v[\d.]+"~appVersion: "v'"${VERSION}"'"~' \
+    --expression 's~appVersion: "v[0-9\.]+"~appVersion: "v'"${VERSION}"'"~' \
         ./charts/fivenet/Chart.yaml
 
 make yarn-upgrade-gen-js
