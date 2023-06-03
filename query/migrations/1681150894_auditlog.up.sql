302936c49cb4f77f2c831b5bfdf843365254bfc6
Alexander Trost <galexrt@googlemail.com>
2023-06-03 11:25:59 +0200

    db: add more indexes to audit log
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1681150894_auditlog.up.sql
+++ query/migrations/1681150894_auditlog.up.sql
@@ -11,7 +11,10 @@ CREATE TABLE IF NOT EXISTS `fivenet_audit_log` (
   `method` varchar(255) NOT NULL,
   `state` smallint(2) NOT NULL,
   `data` longtext DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `idx_fivenet_audit_log_user_id` (`user_id`),
+  KEY `idx_fivenet_audit_log_user_job` (`user_job`),
+  KEY `idx_fivenet_audit_log_created_at` (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 COMMIT;
