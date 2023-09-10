b122bc2c2bbd7bd516117d2d4c7971272270b4ad
Alexander Trost <galexrt@googlemail.com>
2023-09-10 12:42:07 +0200

    fix: char sex badge showing wrong icon
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/citizens/CharSexBadge.vue
+++ src/components/citizens/CharSexBadge.vue
@@ -11,13 +11,13 @@ defineProps<{
         v-if="sex === 'f'"
         class="bg-pink-200 text-pink-600 inline-flex items-center rounded-full px-2.5 py-0.5 text-sm font-medium"
     >
-        <GenderMaleIcon class="h-5 w-5" />
+        <GenderFemaleIcon class="h-5 w-5" />
     </span>
 
     <span
         v-else-if="sex === 'm'"
         class="bg-blue-200 text-blue-600 inline-flex items-center rounded-full px-2 py-0.25 text-sm font-medium"
     >
-        <GenderFemaleIcon class="h-5 w-5" />
+        <GenderMaleIcon class="h-5 w-5" />
     </span>
 </template>
