5a7a69790c0bedd577add2fd6ca82bf81165bc5c
Alexander Trost <galexrt@googlemail.com>
2023-09-18 15:36:05 +0200

    fix: content center wrapper not correctly centering
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -32,7 +32,7 @@ watch(accessToken, async (): Promise<NavigationFailure | TypedRouteFromName<'aut
 </script>
 
 <template>
-    <div class="max-w-lg mx-auto">
+    <div class="max-w-lg sm:min-w-[32rem] mx-auto">
         <div class="px-4 py-8 rounded-lg bg-base-800 sm:px-10">
             <FiveNetLogo class="h-auto mx-auto mb-2 w-36" />
 
--- src/components/partials/ContentCenterWrapper.vue
+++ src/components/partials/ContentCenterWrapper.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="flex w-full h-full">
-        <div class="m-auto py-2 w-full">
+        <div class="my-auto max-sm:w-full sm:mx-auto lg:mx-auto py-2">
             <slot></slot>
         </div>
     </main>
