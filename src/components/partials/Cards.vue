08513403704a49b344012550afe4e8f2129a331e
Alexander Trost <galexrt@googlemail.com>
2023-10-06 03:11:16 +0200

    fix: margin/padding on cards
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -13,55 +13,57 @@ defineEmits<{
 </script>
 
 <template>
-    <div
-        class="w-full overflow-hidden divide-y-4 rounded-lg bg-base-900 shadow-float sm:grid sm:gap-1 sm:px-2 divide-accent-900 sm:divide-y-0"
-        :class="[items.length === 1 ? '' : 'sm:grid-cols-2 sm:max-w-6xl']"
-    >
-        <template v-for="(item, itemIdx) in items">
-            <div
-                v-if="can(item.permission ?? '')"
-                :key="item.title"
-                :class="[
-                    itemIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
-                    itemIdx === 1 ? 'sm:rounded-tr-lg' : '',
-                    itemIdx === items.length - 2 && items.length % 2 === 0 ? 'sm:rounded-bl-lg' : '',
-                    itemIdx === items.length - 1 && items.length % 2 === 0 ? 'rounded-br-lg' : '',
-                    itemIdx === items.length - 1 ? 'rounded-bl-lg sm:rounded-bl-none' : '',
-                    'group relative bg-base-700 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-neutral',
-                ]"
-            >
-                <div v-if="item.icon">
-                    <span :class="[item.iconBackground, item.iconForeground, 'inline-flex rounded-lg p-3']">
-                        <component :is="item.icon" class="h-auto w-7" aria-hidden="true" />
-                    </span>
-                </div>
-                <div class="mt-4" @click="$emit('selected', itemIdx)">
-                    <h3 class="text-base font-semibold leading-6 text-neutral">
-                        <template v-if="item.href">
-                            <NuxtLink :to="item.href" class="focus:outline-none">
+    <div class="sm:px-2">
+        <div
+            class="w-full overflow-hidden divide-y-4 rounded-lg bg-base-900 shadow-float sm:grid sm:gap-1 divide-accent-900 sm:divide-y-0"
+            :class="[items.length === 1 ? '' : 'sm:grid-cols-2 sm:max-w-6xl']"
+        >
+            <template v-for="(item, itemIdx) in items">
+                <div
+                    v-if="can(item.permission ?? '')"
+                    :key="item.title"
+                    :class="[
+                        itemIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+                        itemIdx === 1 ? 'sm:rounded-tr-lg' : '',
+                        itemIdx === items.length - 2 && items.length % 2 === 0 ? 'sm:rounded-bl-lg' : '',
+                        itemIdx === items.length - 1 && items.length % 2 === 0 ? 'rounded-br-lg' : '',
+                        itemIdx === items.length - 1 ? 'rounded-bl-lg sm:rounded-bl-none' : '',
+                        'group relative bg-base-700 p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-neutral',
+                    ]"
+                >
+                    <div v-if="item.icon">
+                        <span :class="[item.iconBackground, item.iconForeground, 'inline-flex rounded-lg p-3']">
+                            <component :is="item.icon" class="h-auto w-7" aria-hidden="true" />
+                        </span>
+                    </div>
+                    <div class="mt-4" @click="$emit('selected', itemIdx)">
+                        <h3 class="text-base font-semibold leading-6 text-neutral">
+                            <template v-if="item.href">
+                                <NuxtLink :to="item.href" class="focus:outline-none">
+                                    <!-- Extend touch target to entire panel -->
+                                    <span class="absolute inset-0" aria-hidden="true" />
+                                    {{ item.title }}
+                                </NuxtLink>
+                            </template>
+                            <template v-else>
                                 <!-- Extend touch target to entire panel -->
                                 <span class="absolute inset-0" aria-hidden="true" />
                                 {{ item.title }}
-                            </NuxtLink>
-                        </template>
-                        <template v-else>
-                            <!-- Extend touch target to entire panel -->
-                            <span class="absolute inset-0" aria-hidden="true" />
-                            {{ item.title }}
-                        </template>
-                    </h3>
-                    <p class="mt-2 text-sm text-base-200">
-                        {{ item.description }}
-                    </p>
+                            </template>
+                        </h3>
+                        <p class="mt-2 text-sm text-base-200">
+                            {{ item.description }}
+                        </p>
+                    </div>
+                    <span
+                        v-if="showIcon"
+                        class="absolute pointer-events-none top-6 right-6 text-base-300 group-hover:text-base-200"
+                        aria-hidden="true"
+                    >
+                        <ChevronRightIcon class="w-6 h-6" />
+                    </span>
                 </div>
-                <span
-                    v-if="showIcon"
-                    class="absolute pointer-events-none top-6 right-6 text-base-300 group-hover:text-base-200"
-                    aria-hidden="true"
-                >
-                    <ChevronRightIcon class="w-6 h-6" />
-                </span>
-            </div>
-        </template>
+            </template>
+        </div>
     </div>
 </template>
