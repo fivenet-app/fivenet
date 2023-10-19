467550da8afdf8e1597c9eb18d201c536df2883a
Alexander Trost <galexrt@googlemail.com>
2023-10-19 21:10:16 +0200

    fix: citizens vehicles list not showing copy vehicle button
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/vehicles/ListEntry.vue
+++ src/components/vehicles/ListEntry.vue
@@ -44,7 +44,7 @@ function addToClipboard(): void {
             <CitizenInfoPopover :user="vehicle.owner" />
         </td>
         <td
-            v-if="!hideCitizenLink && !hideCopy"
+            v-if="!hideCitizenLink || !hideCopy"
             class="whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
         >
             <div class="flex flex-row justify-end">
