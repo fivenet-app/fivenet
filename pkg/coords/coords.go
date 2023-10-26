37a9e549b3808a4aad5692b51b9e1b597f45318d
Alexander Trost <galexrt@googlemail.com>
2023-10-26 20:33:46 +0200

    fix: use normal mutex for coords
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/coords/coords.go
+++ pkg/coords/coords.go
@@ -12,7 +12,7 @@ import (
 type CoordsKeyFn[K comparable] func(orb.Point) K
 
 type Coords[K comparable, V any] struct {
-	mutex       sync.RWMutex
+	mutex       sync.Mutex
 	tree        *quadtree.Quadtree
 	targets     *xsync.MapOf[K, V]
 	coordsKeyFn CoordsKeyFn[K]
@@ -21,7 +21,7 @@ type Coords[K comparable, V any] struct {
 func New[K comparable, V any](fn CoordsKeyFn[K]) *Coords[K, V] {
 	tree := quadtree.New(orb.Bound{Min: orb.Point{-4000, -4000}, Max: orb.Point{8000, 6000}})
 	return &Coords[K, V]{
-		mutex:       sync.RWMutex{},
+		mutex:       sync.Mutex{},
 		tree:        tree,
 		targets:     xsync.NewMapOf[K, V](),
 		coordsKeyFn: fn,
@@ -29,8 +29,8 @@ func New[K comparable, V any](fn CoordsKeyFn[K]) *Coords[K, V] {
 }
 
 func (p *Coords[K, V]) Has(x float64, y float64) (ok bool) {
-	p.mutex.RLock()
-	defer p.mutex.RUnlock()
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
 	_, ok = p.targets.Load(p.coordsKeyFn(orb.Point{x, y}))
 	return
 }
@@ -57,16 +57,16 @@ func (p *Coords[K, V]) Closest(x, y float64) (V, bool) {
 	nearest := p.tree.Find(orb.Point{x, y})
 	key := p.coordsKeyFn(nearest.Point())
 
-	p.mutex.RLock()
-	defer p.mutex.RUnlock()
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
 	return p.targets.Load(key)
 }
 
 func (p *Coords[K, V]) KNearest(point orb.Point, max int, maxDistance float64) []V {
 	points := p.tree.KNearest(nil, point, max, maxDistance)
 
-	p.mutex.RLock()
-	defer p.mutex.RUnlock()
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
 	results := make([]V, len(points))
 	for i := 0; i < len(points); i++ {
 		key := p.coordsKeyFn(points[i].Point())
