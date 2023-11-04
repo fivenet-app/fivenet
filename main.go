40252e8717eb072d772c2b7ecabd168267570043
Alexander Trost <galexrt@googlemail.com>
2023-11-04 15:37:33 +0100

    fix: increase start delay
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
 			return &fxevent.ZapLogger{Logger: log}
 		}),
-		fx.StartTimeout(60 * time.Second),
+		fx.StartTimeout(90 * time.Second),
 
 		LoggerModule,
 		config.Module,
