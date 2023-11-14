388cd879f896a0f51b76d8dbebfff2069175bc6b
Alexander Trost <galexrt@googlemail.com>
2023-11-14 22:51:01 +0100

    fix: update centrum state comment
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- gen/go/proto/services/centrum/state/settings.go
+++ gen/go/proto/services/centrum/state/settings.go
@@ -20,6 +20,7 @@ func (s *State) GetSettings(job string) *dispatch.Settings {
 
 func (s *State) UpdateSettings(job string, in *dispatch.Settings) error {
 	current := s.GetSettings(job)
+	// Simply use protobuf merge to update existing settings with incoming settings
 	proto.Merge(current, in)
 
 	return nil
