6922dd417b86ac276ad12f9d04ad37d76185ca65
Alexander Trost <galexrt@googlemail.com>
2023-11-28 14:42:18 +0100

    fix: margin/padding issue with activity list disclosure
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/partials/data/DataNoDataBlock.vue
+++ src/components/partials/data/DataNoDataBlock.vue
@@ -25,20 +25,22 @@ function click() {
 </script>
 
 <template>
-    <button
-        type="button"
-        :disabled="!focus"
-        class="mt-4 relative block w-full p-12 text-center border-2 border-dashed rounded-lg border-base-300 hover:border-base-400 focus:outline-none focus:ring-2 focus:ring-neutral focus:ring-offset-2"
-        @click="click()"
-    >
-        <component :is="icon" class="w-12 h-12 mx-auto text-neutral" />
-        <span class="block mt-2 text-sm font-semibold text-gray-300">
-            <span v-if="message">
-                {{ message }}
+    <div class="pt-4">
+        <button
+            type="button"
+            :disabled="!focus"
+            class="relative block w-full p-12 text-center border-2 border-dashed rounded-lg border-base-300 hover:border-base-400 focus:outline-none focus:ring-2 focus:ring-neutral focus:ring-offset-2"
+            @click="click()"
+        >
+            <component :is="icon" class="w-12 h-12 mx-auto text-neutral" />
+            <span class="block mt-2 text-sm font-semibold text-gray-300">
+                <span v-if="message">
+                    {{ message }}
+                </span>
+                <span v-else>
+                    {{ $t('common.not_found', [type ?? $t('common.data')]) }}
+                </span>
             </span>
-            <span v-else>
-                {{ $t('common.not_found', [type ?? $t('common.data')]) }}
-            </span>
-        </span>
-    </button>
+        </button>
+    </div>
 </template>
