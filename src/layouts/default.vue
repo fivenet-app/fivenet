c8944e1e79851ca4089e1e7d58326f0d425f58cc
Alexander Trost <galexrt@googlemail.com>
2023-12-24 22:26:47 +0100

    fix: adjust snowflakes event days
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -11,7 +11,7 @@ const { activeChar } = storeToRefs(authStore);
 
 // Use client date to show any event overlays
 const now = new Date();
-const showSnowflakes = now.getMonth() + 1 === 12 && (now.getDate() > 20 || now.getDate() < 26);
+const showSnowflakes = now.getMonth() + 1 === 12 && (now.getDate() >= 21 || now.getDate() <= 26);
 </script>
 
 <template>
