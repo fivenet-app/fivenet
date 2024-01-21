bf3ae74b9c6c6ffe8aa3071ad39c8ca82c39cfaa
Alexander Trost <galexrt@googlemail.com>
2024-01-21 21:15:53 +0100

    fix: conduct list even/odd background color
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- src/components/jobs/conduct/ConductListEntry.vue
+++ src/components/jobs/conduct/ConductListEntry.vue
@@ -18,7 +18,7 @@ const openMessage = ref(false);
 </script>
 
 <template>
-    <tr class="transition-colors hover:bg-neutral/5">
+    <tr class="transition-colors even:bg-base-800 hover:bg-neutral/5">
         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-base font-medium text-neutral sm:pl-1">
             <GenericTime :value="conduct.createdAt" />
         </td>
