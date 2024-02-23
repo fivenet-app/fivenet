a5b5207728589b34165839e609d9a303f91dddf6
Alexander Trost <galexrt@googlemail.com>
2024-02-23 01:32:17 +0100

    fix: image proxy not being enabled breaking http server
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/server/images/proxy.go
+++ pkg/server/images/proxy.go
@@ -12,29 +12,29 @@ import (
 type ImageProxy struct {
 	logger *zap.Logger
 
-	options config.ImageProxyOptions
+	config config.ImageProxy
 }
 
 func New(logger *zap.Logger, cfg *config.Config) *ImageProxy {
-	if !cfg.ImageProxy.Enabled {
-		return nil
-	}
-
 	ip := &ImageProxy{
-		logger:  logger,
-		options: cfg.ImageProxy.Options,
+		logger: logger,
+		config: cfg.ImageProxy,
 	}
 
 	return ip
 }
 
 func (p *ImageProxy) RegisterHTTP(e *gin.Engine) {
+	if !p.config.Enabled {
+		return
+	}
+
 	// Image Proxy
 	proxy := imageproxy.NewProxy(http.DefaultTransport, imageproxy.NopCache)
 	proxy.Logger = zap.NewStdLog(p.logger.Named("image_proxy"))
 
-	proxy.AllowHosts = p.options.AllowHosts
-	proxy.DenyHosts = p.options.DenyHosts
+	proxy.AllowHosts = p.config.Options.AllowHosts
+	proxy.DenyHosts = p.config.Options.DenyHosts
 
 	// Example URL: http://localhost:3000/api/image_proxy/500/https://octodex.github.com/images/codercat.jpg
 	e.GET("/api/image_proxy/*url", gin.WrapH(http.StripPrefix("/api/image_proxy", proxy)))
