0799e92459dadd671da6c78b23b455165576730a
Alexander Trost <galexrt@googlemail.com>
2024-04-15 15:46:56 +0200

    fix: slices difference test
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/utils/slices_test.go
+++ pkg/utils/slices_test.go
@@ -11,8 +11,8 @@ func TestSlicesDifference(t *testing.T) {
 	b := []string{"hello", "world", "test1", "abc"}
 
 	added, removed := SlicesDifference(a, b)
-	assert.Contains(t, []string{"world", "test1"}, added)
-	assert.Contains(t, []string{"example"}, removed)
+	assert.ElementsMatch(t, []string{"world", "test1"}, added)
+	assert.ElementsMatch(t, []string{"example"}, removed)
 
 	a = []string{"hello", "world", "abc"}
 	b = []string{"hello", "world", "abc"}
