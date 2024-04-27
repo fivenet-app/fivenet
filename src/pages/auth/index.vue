7dbcae9dbfa236393cd7b1380a4eb4ce799ed6f6
Alexander Trost <galexrt@googlemail.com>
2024-04-27 13:20:44 +0200

    feat: add /auth index redirect page
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ src/pages/auth/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+useHead({
+    title: 'components.auth.LoginForm.title',
+});
+definePageMeta({
+    title: 'components.auth.LoginForm.title',
+    requiresAuth: false,
+    redirect: { name: 'auth-login' },
+});
+</script>
+
+<template>
+    <div></div>
+</template>
