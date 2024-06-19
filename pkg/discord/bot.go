c789e88929c92bd511d7ac997a556eb993d1fc25
Alexander Trost <galexrt@googlemail.com>
2024-06-20 00:03:15 +0200

    fix: discord bot guild select query
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- pkg/discord/bot.go
+++ pkg/discord/bot.go
@@ -347,7 +347,6 @@ func (b *Bot) getJobGuildsFromDB(ctx context.Context) (map[string]string, error)
 		FROM(tJobProps).
 		WHERE(jet.AND(
 			tJobProps.DiscordGuildID.IS_NOT_NULL(),
-			tJobProps.Job.EQ(jet.String("ambulance")),
 		))
 
 	var dest []*struct {
