3582b641df97e0f3dcc66a58314367150c19ecec
Alexander Trost <galexrt@googlemail.com>
2024-10-20 16:24:08 +0200

    fix: qualifications exam response changes migration
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1729431210_qualifications_exam_response_changes.up.sql
+++ query/migrations/1729431210_qualifications_exam_response_changes.up.sql
@@ -1,13 +1,13 @@
 BEGIN;
 
-ALTER TABLE `fivenet_qualifications_exam_responses` DROP FOREIGN KEY fk_fivenet_qualifications_exam_responses_question_id;
+ALTER TABLE `fivenet_qualifications_exam_responses` DROP FOREIGN KEY `fk_fivenet_qualifications_exam_responses_question_id`;
 ALTER TABLE `fivenet_qualifications_exam_responses` DROP INDEX `PRIMARY`;
 ALTER TABLE `fivenet_qualifications_exam_responses` DROP `question_id`;
-ALTER TABLE `fivenet_qualifications_exam_responses` ADD qualification_id BIGINT UNSIGNED NOT NULL;
-ALTER TABLE `fivenet_qualifications_exam_responses` CHANGE qualification_id qualification_id BIGINT UNSIGNED NOT NULL FIRST;
-ALTER TABLE `fivenet_qualifications_exam_responses` CHANGE response responses longtext NULL;
+ALTER TABLE `fivenet_qualifications_exam_responses` ADD `qualification_id` BIGINT UNSIGNED NOT NULL;
+ALTER TABLE `fivenet_qualifications_exam_responses` CHANGE `qualification_id` `qualification_id` BIGINT UNSIGNED NOT NULL FIRST;
+ALTER TABLE `fivenet_qualifications_exam_responses` CHANGE `response` `responses` longtext NULL;
 ALTER TABLE `fivenet_qualifications_exam_responses` ADD PRIMARY KEY (`qualification_id`, `user_id`);
-ALTER TABLE `fivenet_qualifications_exam_responses` ADD CONSTRAINT fivenet_qualifications_exam_responses_quali_user_id FOREIGN KEY (`qualification_id`) REFERENCES `fivenet_qualifications` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `fivenet_qualifications_exam_responses` ADD CONSTRAINT fivenet_qualifications_exam_responses_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `fivenet_qualifications_exam_responses` ADD CONSTRAINT `fivenet_qualifications_exam_responses_quali_user_id` FOREIGN KEY (`qualification_id`) REFERENCES `fivenet_qualifications` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `fivenet_qualifications_exam_responses` ADD CONSTRAINT `fivenet_qualifications_exam_responses_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 COMMIT;
