504d669b1aa3595af717498194eeb531d5f6b648
Alexander Trost <galexrt@googlemail.com>
2024-11-10 15:07:09 +0100

    fix: perms consolidation missing one
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1731246646_perms_consolidation.up.sql
+++ query/migrations/1731246646_perms_consolidation.up.sql
@@ -3,5 +3,6 @@ BEGIN;
 DELETE FROM fivenet_permissions WHERE category = 'DocStoreService' AND name = 'GetDocument' LIMIT 1;
 DELETE FROM fivenet_permissions WHERE category = 'QualificationsService' AND name = 'GetQualification' LIMIT 1;
 DELETE FROM fivenet_permissions WHERE category = 'WikiService' AND name = 'GetPage' LIMIT 1;
+DELETE FROM fivenet_permissions WHERE category = 'CompletorService' AND name = 'CompleteJobs' LIMIT 1;
 
 COMMIT;
