cd97c2a97b5d0190ea7ef037d28ee43ea5563c16
Alexander Trost <galexrt@googlemail.com>
2025-01-30 21:25:18 +0100

    fix: remove Array.prototype.toSorted and use typescript "native"
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- app/plugins/polyfills.client.ts
+++ app/plugins/polyfills.client.ts
@@ -1,3 +1,17 @@
-import 'array.prototype.tosorted/auto';
+export default defineNuxtPlugin(() => {
+    if (!import.meta.client) {
+        return;
+    }
 
-export default defineNuxtPlugin(() => {});
+    // toSorted polyfill
+    if (!Array.prototype.toSorted) {
+        console.info('Adding Polyfill');
+        Object.defineProperty(Array.prototype, 'toSorted', {
+            value: function <T>(this: T[], compareFn?: (a: T, b: T) => number): T[] {
+                return [...this].sort(compareFn);
+            },
+            writable: true,
+            configurable: true,
+        });
+    }
+});
