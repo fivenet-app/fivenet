2b24422136b06bfe4244e9df63a2935eb9f50c0f
Alexander Trost <galexrt@googlemail.com>
2025-03-02 20:51:36 +0100

    fix: dbsync invoking croner and server component
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- cmd/dbsync.go
+++ cmd/dbsync.go
@@ -8,7 +8,7 @@ import (
 type DBSyncCmd struct{}
 
 func (c *DBSyncCmd) Run(ctx *Context) error {
-	fxOpts := getFxBaseOpts(Cli.StartTimeout, true)
+	fxOpts := getFxBaseOpts(Cli.StartTimeout, false)
 	fxOpts = append(fxOpts, fx.Invoke(func(*dbsync.Sync) {}))
 
 	app := fx.New(fxOpts...)
