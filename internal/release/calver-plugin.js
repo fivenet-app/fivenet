2844e4774e2c2807c66915b434fc6d2883e8a482
Alexander Trost <galexrt@googlemail.com>
2025-04-22 21:05:03 +0200

    fix: tweak release-it config
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ internal/release/calver-plugin.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import calver from 'calver/node/lts';
+import { Plugin } from 'release-it';
+const DEFAULT_FORMAT = 'yyyy.0m.minor',
+    DEFAULT_INCREMENT = 'calendar',
+    FALLBACK_INCREMENT = 'minor';
+
+class CalverPlugin extends Plugin {
+    getFormat() {
+        return this.getContext().format || DEFAULT_FORMAT;
+    }
+
+    getInc() {
+        return this.getContext().increment || DEFAULT_INCREMENT;
+    }
+
+    getFallbackInc() {
+        return this.getContext().fallbackIncrement || FALLBACK_INCREMENT;
+    }
+
+    getIncrementedVersion(args) {
+        const { latestVersion } = args || {};
+        try {
+            return calver.inc(this.getFormat(), latestVersion, this.getInc());
+        } catch {
+            try {
+                return calver.inc(this.getFormat(), latestVersion, this.getFallbackInc());
+            } catch {
+                return latestVersion;
+            }
+        }
+    }
+
+    getIncrementedVersionCI() {
+        return this.getIncrementedVersion(...arguments);
+    }
+
+    getIncrement() {
+        return this.getIncrementedVersion(...arguments);
+    }
+}
+
+export default CalverPlugin;
