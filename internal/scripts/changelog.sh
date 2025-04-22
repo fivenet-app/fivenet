64ca65895c6787227fa6c0463991dd77b5603bf6
Alexander Trost <galexrt@googlemail.com>
2025-04-22 23:59:02 +0200

    fix: git-cliff config skipping the wrong tags
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- internal/scripts/changelog.sh
+++ internal/scripts/changelog.sh
@@ -3,7 +3,7 @@
 NODE_VERSION=$(node -p -e "require('./package.json').version")
 
 if [ "$1" = "stdout" ]; then
-    pnpm git-cliff -o - --unreleased --tag "$NODE_VERSION"
+    pnpx git-cliff -o - --unreleased --tag "$NODE_VERSION"
 else
-    pnpm git-cliff -o './CHANGELOG.md' --tag "$NODE_VERSION"
+    pnpx git-cliff -o './CHANGELOG.md' --tag "$NODE_VERSION"
 fi
