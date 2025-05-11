74663b2a71f148d99196f99c36543f0553fe83cd
Alexander Trost <galexrt@googlemail.com>
2025-05-11 15:40:22 +0200

    fix: change perm id of job limit perms for category create
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1746969206_categories_perm_change.up.sql
+++ query/migrations/1746969206_categories_perm_change.up.sql
@@ -13,6 +13,19 @@ UPDATE `fivenet_role_permissions` SET `permission_id` = (
 WHERE
 	`permission_id` = (SELECT `id` FROM `fivenet_permissions` WHERE `category` = 'DocStoreService' AND name = 'CreateCategory' LIMIT 1);
 
+UPDATE `fivenet_job_permissions` SET `permission_id` = (
+	SELECT
+		id
+	FROM
+		fivenet_permissions
+	WHERE
+		category = 'DocStoreService'
+		AND name = 'CreateOrUpdateCategory'
+	LIMIT 1
+)
+WHERE
+	`permission_id` = (SELECT `id` FROM `fivenet_permissions` WHERE `category` = 'DocStoreService' AND name = 'CreateCategory' LIMIT 1);
+
 DELETE FROM `fivenet_permissions` WHERE `category` = 'DocStoreService' AND `name` = 'CreateCategory' LIMIT 1;
 
 COMMIT;
