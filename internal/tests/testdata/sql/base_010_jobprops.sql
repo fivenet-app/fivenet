02c52fba4c7fd6d57c4abdb37453ad757b5ee5f3
Alexander Trost <galexrt@googlemail.com>
2025-05-19 14:58:53 +0200

    fix: test failures caused by test data inserting job prop theme column
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- internal/tests/testdata/sql/base_010_jobprops.sql
+++ internal/tests/testdata/sql/base_010_jobprops.sql
@@ -1,7 +1,7 @@
 -- Table Data: fivenet_job_props
 INSERT INTO fivenet_job_props
-(job, theme, livemap_marker_color, quick_buttons, discord_sync_settings)
-VALUES('ambulance', 'default', '9bc9ff', '{}', '{"user_info_sync":false}');
+(job, livemap_marker_color, quick_buttons, discord_sync_settings)
+VALUES('ambulance', '9bc9ff', '{}', '{"user_info_sync":false}');
 INSERT INTO fivenet_job_props
-(job, theme, livemap_marker_color, quick_buttons, discord_sync_settings)
-VALUES('police', 'default', 'F106E2', '{"penalty_calculator":true}', '{"user_info_sync":false}');
+(job, livemap_marker_color, quick_buttons, discord_sync_settings)
+VALUES('police', 'F106E2', '{"penalty_calculator":true}', '{"user_info_sync":false}');
