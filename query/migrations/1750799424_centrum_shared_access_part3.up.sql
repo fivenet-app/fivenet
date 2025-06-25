784671607aa7138c0b096d75938f3b2862590915
Alexander Trost <galexrt@googlemail.com>
2025-06-25 20:13:27 +0200

    fix: skip of mariadb incompatible json index
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1750799424_centrum_shared_access_part3.up.sql
+++ query/migrations/1750799424_centrum_shared_access_part3.up.sql
@@ -6,17 +6,22 @@ ALTER TABLE `fivenet_centrum_dispatches` ADD CONSTRAINT `chk_fivenet_centrum_dis
 
 -- MariaDB doesn't support JSON multi-valued indexes, so we skip this part
 -- It might land in MariaDB 12.2 or later, see https://jira.mariadb.org/browse/MDEV-25848
-SET @ver := @@version_comment;
+SET @ver := VERSION();
 
+-- Build either the ALTER or a no-op SELECT
 SET @ddl :=
   IF(
     LOCATE('MariaDB', @ver) = 0,
-    'ALTER TABLE `fivenet_centrum_dispatches`
-       ADD INDEX `idx_jobs`
-         ((CAST(`jobs` AS CHAR(255) ARRAY)));',
-    'SELECT "Skipping JSON multi-valued index on MariaDB: '
-      || @ver || '"'
+    'ALTER TABLE your_table
+       ADD INDEX idx_jobs_elements
+         ((CAST(jobs AS CHAR(255) ARRAY)));',
+    CONCAT(
+      'SELECT ''Skipping JSON multi-valued index on MariaDB: ',
+      @ver,
+      ''';'
+    )
   );
+
 PREPARE stmt FROM @ddl;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
