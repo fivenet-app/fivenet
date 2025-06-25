f597a6f0d085d9cbea48e68c33608e84fcdca1a3
Alexander Trost <galexrt@googlemail.com>
2025-06-25 10:44:40 +0200

    fix: migration for mariadb
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- query/migrations/1750799424_centrum_shared_access_part3.up.sql
+++ query/migrations/1750799424_centrum_shared_access_part3.up.sql
@@ -3,6 +3,22 @@ BEGIN;
 ALTER TABLE `fivenet_centrum_dispatches` DROP INDEX `idx_fivenet_centrum_dispatches_job`;
 ALTER TABLE `fivenet_centrum_dispatches` MODIFY COLUMN `jobs` json NOT NULL;
 ALTER TABLE `fivenet_centrum_dispatches` ADD CONSTRAINT `chk_fivenet_centrum_dispatches_jobs` CHECK (json_valid(`jobs`));
-ALTER TABLE `fivenet_centrum_dispatches` ADD INDEX `idx_jobs` ((CAST(`jobs` AS CHAR(255) ARRAY)));
+
+-- MariaDB doesn't support JSON multi-valued indexes, so we skip this part
+-- It might land in MariaDB 12.2 or later, see https://jira.mariadb.org/browse/MDEV-25848
+SET @ver := @@version_comment;
+
+SET @ddl :=
+  IF(
+    LOCATE('MariaDB', @ver) = 0,
+    'ALTER TABLE `fivenet_centrum_dispatches`
+       ADD INDEX `idx_jobs`
+         ((CAST(`jobs` AS CHAR(255) ARRAY)));',
+    'SELECT "Skipping JSON multi-valued index on MariaDB: '
+      || @ver || '"'
+  );
+PREPARE stmt FROM @ddl;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
 COMMIT;
