9f7644f66da462ed306bde19f8bf6c4dca5e3c44
Alexander Trost <galexrt@googlemail.com>
2025-10-19 18:10:37 +0200

    fix: add missing coming soon label to create stamps button
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- app/pages/documents/stamps.vue
+++ app/pages/documents/stamps.vue
@@ -55,8 +55,8 @@ async function listApprovalTasks(): Promise<ListUsableStampsResponse> {
                 <template #right>
                     <PartialsBackButton fallback-to="/documents" />
 
-                    <UTooltip v-if="can('documents.StampsService/UpsertStamp').value" :text="$t('common.create')">
-                        <UButton trailing-icon="i-mdi-plus" color="neutral" truncate>
+                    <UTooltip v-if="can('documents.StampsService/UpsertStamp').value" :text="$t('common.coming_soon')">
+                        <UButton trailing-icon="i-mdi-plus" color="neutral" truncate disabled>
                             <span class="hidden truncate sm:block">
                                 {{ $t('common.stamp', 1) }}
                             </span>
