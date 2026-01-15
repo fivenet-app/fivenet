6ac88aaa482a0e4673c71c54744d5f0a7c37f2b0
Alexander Trost <galexrt@googlemail.com>
2026-01-16 00:35:14 +0100

    chore: nix flake update
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "Basic flake for FiveNet development";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.11";
   };
 
   outputs = { self, nixpkgs }:
